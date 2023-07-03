e1c6de1c6c1f550c08c939ed01b4f4b0c4f34bc8
Crowdbotics <team@crowdbotics.com>
2023-07-03 01:45:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solitary-recipe-41872",
+  url: "https://solitary-recipe-41872.botics.co",
+  api: "https://solitary-recipe-41872.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
